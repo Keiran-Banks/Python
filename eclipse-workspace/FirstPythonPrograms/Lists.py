93cf055f6ae572480a87c090bc7b27604b2c0b38
Keiran <keiran@gudgeon.net>
2017-09-14 17:53:11 +0100

    First 4 projects

---
+++ eclipse-workspace/FirstPythonPrograms/Lists.py
@@ -0,0 +1,21 @@
+mylist = []
+mylist.append(1)
+mylist.append(2)
+mylist.append(3)
+print(mylist[0])
+print(mylist[1])
+print(mylist[2])
+
+for x in mylist:
+    print(x)
+    
+numbers = [1,2,3]
+strings = ['hello', 'world']
+names = ["John", "Eric", "Jessica"]
+
+second_name = ["Smith", "Jones", "Adams"]
+
+print(numbers)
+print(strings)
+
+print("The second name on the list is %s" % names[1] + " " + second_name[1])
